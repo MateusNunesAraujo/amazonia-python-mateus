65453ce5c82522d42194f172ff93389c6bd3bfee
Nico140406 <nico070123@gmail.com>
2026-04-28 18:33:18 -0500

    Subiendo nuevos cambios

--- amazonia.py
+++ amazonia.py
@@ -4,7 +4,7 @@
 animales = [
     {"nombre": "Jaguar", "dato": " Es el felino mas grande de America."},
     {"nombre": "America Monkey", "dato": "It's a America Monkey from Africa"},
-    {"nombre": "Caiman", "dato": "Es un caiman"}
+    {"nombre": "Caiman", "dato": "Es un caiman"},
      { "nombre": "Parrot","dato":"I would like to have a parrot because they're realy beautiful"}
 ]
 print("=== Animales de la Amazonia ===")
